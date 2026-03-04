6791c82af2eaccb0974c7c6a0d5c3c69f289353b
natewebman <natecorrieri@gmail.com>
2026-03-04 13:57:57 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  site: 'https://classactionlawupdates.com',
-  integrations: [tailwind()],
-  output: 'hybrid',
+    site: 'https://classactionlawupdates.com',
+    integrations: [tailwind()],
+    output: 'hybrid',
+    adapter: cloudflare(),
 });
